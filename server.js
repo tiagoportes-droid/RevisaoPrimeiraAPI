41aa917cca2d92dde229a90d2ab3d3e44da49701
TIAGO FERREIRA DA LUZ PORTES <26171637@edu119.local>
2026-09-15 10:45:14 -0300

    feat: more add in sever.js

--- server.js
+++ server.js
@@ -6,17 +6,33 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // {
 //     "nome": "Pedro",
 //     "curso": "Desenvolvimento de Sistemas"
 // }
 
+let ALUNOS = [
+  { id: 1, nome: "Alice", curso: "Desenvolvimento de Sistemas" },
+  { id: 2, nome: "Brenda", curso: "Rede de Computadores" },
+  { id: 3, nome: "Brenno", curso: "Administração" },
+  { id: 4, nome: "Carlos", curso: "Desenvolvimento de Sistemas" },
+];
 
-let alunos = [
-    {id: 2, nome: "Alice", curso: "Desenvolvimento de Sistemas" },
-    {id: 3, nome: "Brenda", curso: "Rede de Computadores"},
-    {id: 1, nome: "Brenno", curso: "Administração" },
-    {id: 4, nome: "Carlos", curso: "Desenvolvimento de Sistemas" },
-]
+app.get("/", (req, res) => {
+  res.json({
+    mensagem: "API alunos funcionando",
+  });
+});
+
+app.get("/alunos", (req, res) => {
+  res.json(ALUNOS);
+});
+
+const PORTA = 3000;
+
+app.listen(PORTA, () => {
+  console.log("Servidor inicado com sucesso");
+  console.log(`http://localhost:${PORTA}`);
+});
