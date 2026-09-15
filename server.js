53c3df1d2e80af063d64725fe39e7c46d65224f8
Tiago Portes <tiago.fportes@gmail.com>
2026-09-15 09:24:04 -0300

    feat: banco de dados

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+// import express from express;
+// import cors from cors;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+// {
+//     "nome": "Pedro",
+//     "curso": "Desenvolvimento de Sistemas"
+// }
+
+
+let alunos = [
+    {id: 2, nome: "Alice", curso: "Desenvolvimento de Sistemas" },
+    {id: 3, nome: "Brenda", curso: "Rede de Computadores"},
+    {id: 1, nome: "Brenno", curso: "Administração" },
+    {id: 4, nome: "Carlos", curso: "Desenvolvimento de Sistemas" },
+]
