d16fbce4fa27304acdd7c7a3884bf8fe39ab3de9
Tiago Portes <tiago.fportes@gmail.com>
2026-09-15 11:37:35 -0300

    feat: add in server

--- server.js
+++ server.js
@@ -30,6 +30,43 @@ app.get("/alunos", (req, res) => {
   res.json(ALUNOS);
 });
 
+app.get("/alunos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const alunos = ALUNOS.find((a) => a.id === id);
+
+  if (!alunos) {
+    return res.status(404).json({
+      mensagem: "Aluno não encontrado",
+    });
+  }
+
+  res.status(200).json(alunos);
+});
+
+app.post("/alunos/cadastrar", (req, res) => {
+  const { nome, curso } = req.body;
+
+  if (!nome || !curso) {
+    return res.status(400).json({
+      mensagem: "Nome e curso são obrigatorios",
+    });
+  }
+
+  const novoId = ALUNOS.length > 0 ? Math.max(...ALUNOS.map((aluno) => aluno.id)) + 1 : 1;
+
+  const novoAluno = {
+    id: novoId,
+    nome: nome,
+    curso: curso
+  };
+
+  ALUNOS.push(novoAluno);
+
+  res.status(201).json({
+    mensagem: "Alunos cadastrado com sucesso",
+  });
+});
+
 const PORTA = 3000;
 
 app.listen(PORTA, () => {
